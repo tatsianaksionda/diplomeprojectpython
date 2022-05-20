ca6e9d507ffe0b70c32e80f95b95cbd6a0c4bf06
tatsianaksionda <105910709+tatsianaksionda@users.noreply.github.com>
2022-05-20 16:35:32 +0300

    Update settings.py

--- RSA/RSA/settings.py
+++ RSA/RSA/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vv8y+#y&avfs%0^4my@7)-u()ci$scoxp87l&-o(ax8)itbp6@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1']
